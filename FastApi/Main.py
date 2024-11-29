6452ecd9e13db2991c7481abf2b0f214ba35f9ba
EdwindanielTIC <2024_edwin.ramirez@iticbcn.cat>
2024-11-29 19:20:54 +0100

    Añadiendo response en vez de HTTException

--- FastApi/Main.py
+++ FastApi/Main.py
@@ -1,6 +1,7 @@
 from typing import Optional
+from urllib import response
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI,status
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,6 +14,12 @@ class Item(BaseModel): ## esto lo conecto con el post
     Apellido: str
     Edad:int
     Direccion : Optional[str] = None
+    
+items = {
+    1: {"id": 25, "name": "Daniel", "apellido": "Ramirez", "edad": 25, "direccion": "Padillas"},
+    2: {"id": 30, "name": "Carlos", "apellido": "Gonzales", "edad": 30, "direccion": "Mostoles"},
+    3: {"id": 45, "name": "Ana", "apellido": "Martinez", "edad": 58, "direccion": "Argentina"},
+}
 
 
 @app.get("/")
@@ -23,13 +30,13 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
-    if item_id not in Item:
-        raise HTTPException (status_code = 404, detail= "Página no encontrada")
-    return {"item": Item[item_id]}
+    if item_id not in items:
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return {"item": items[item_id]}
 
 ## CREANDO POST
 
-@app.post("/Items")
+@app.post("/Items/")
 async def creando_items(item:Item):
     return item
 
